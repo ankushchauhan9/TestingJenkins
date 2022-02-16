1b53398712d03a33397f7de0ec0575bff34e9fea
ankushchauhan9 <ankushchauhanbtech@gmail.com>
2022-02-16 17:21:34 +0530

    first commit

---
+++ src/test/java/com/example/TestingJenkins/TestingJenkinsApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.TestingJenkins;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+    public void test_1(){
+        Assertions.assertEquals(1,1);
+    }
+
+
+    @Test
+    public void test_2(){
+        Assertions.assertEquals(2,2);
+    }
+
+
+}
